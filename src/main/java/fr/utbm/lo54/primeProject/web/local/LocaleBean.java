f1409de5f5b744c3238c4836415c02d5120a7666
vivictor14 <victor.vogel@hotmail.fr>
2017-06-13 13:12:58 +0200

    I18N

--- src/main/java/fr/utbm/lo54/primeProject/web/local/LocaleBean.java
+++ src/main/java/fr/utbm/lo54/primeProject/web/local/LocaleBean.java
@@ -3,43 +3,46 @@ package fr.utbm.lo54.primeProject.web.local;
  
 import java.io.*;
 import java.util.*;
+import javax.annotation.PostConstruct;
 import javax.faces.bean.*;
 import javax.faces.context.*;
 import javax.faces.event.*;
  
-@ManagedBean
+@ManagedBean(name = "localeManagementBean")
 @SessionScoped
 public class LocaleBean implements Serializable {
-  private final Locale ENGLISH = Locale.ENGLISH;
-  private final Locale SPANISH = new Locale("es");
-  private final Locale FRENCH = Locale.FRENCH;
-  private final Locale BANGLA = new Locale("bn");
-  private Locale locale = ENGLISH;
- 
-  public Locale getLocale() {
-    return(locale);
-  }
-  public void setBangla(ActionEvent event) {
-    locale = BANGLA;
-    updateViewLocale();
-  }
-   
-  public void setEnglish(ActionEvent event) {
-    locale = ENGLISH;
-    updateViewLocale();
-  }
-   
-  public void setSpanish(ActionEvent event) {
-    locale = SPANISH;
-    updateViewLocale();
-  }
-   
-  public void setFrench(ActionEvent event) {
-    locale = FRENCH;
-    updateViewLocale();
-  }
-   
-  private void updateViewLocale() {
-    FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
-  }
+    private Locale locale = Locale.ENGLISH;
+    private String language = "en";
+
+    private List<String> availableLanguages = new ArrayList<>();
+
+    @PostConstruct
+    public void init() {
+        FacesContext.getCurrentInstance().getApplication().getSupportedLocales().forEachRemaining(supportedLocale -> availableLanguages.add(supportedLocale.toLanguageTag()));
+    }
+
+    public Locale getLocale() {
+        return(locale);
+    }
+
+    public void valueChanged(AjaxBehaviorEvent event) {
+        locale = Locale.forLanguageTag(language);
+        updateViewLocale();
+    }
+
+    private void updateViewLocale() {
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public Collection<String> getAvailableLanguages() {
+        return availableLanguages;
+    }
 }
